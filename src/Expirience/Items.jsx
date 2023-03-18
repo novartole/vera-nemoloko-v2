3f0759803d668c9f8af5d20c928b0e65d086a718
Artem Novikov <novartole@gmail.com>
2023-03-18 14:02:22 +0000

    Added items section and one generic item.

---
+++ src/Expirience/Items.jsx
@@ -0,0 +1,134 @@
+import { useFrame, useThree } from '@react-three/fiber';
+import { useEffect, useMemo, useState, useCallback, useLayoutEffect } from "react";
+import { useScroll, Center } from '@react-three/drei';
+import { useControls } from 'leva';
+import * as THREE from 'three';
+import { useSnapshot } from 'valtio';
+
+import Item from "./Models/Item.jsx";
+import { state as globalState } from './util.js';
+
+export default function Items() {
+  console.log('items');
+
+  const scroll = useScroll();
+  const reservedRange = 1 / (scroll.pages + 1); 
+
+  const getThree = useThree(state => state.get);
+
+  const snap = useSnapshot(globalState);
+  
+  const { 
+    cameraRadius,
+    cameraRadiusOffset,
+    cameraRadiusOffsetFactor,
+
+    bla
+  } = useControls(
+    'Items',
+    {
+      cameraRadius: { value: 15, min: 5 , max: 30 },
+      cameraRadiusOffset: { value: 0, min: 0, max: 30 },
+      cameraRadiusOffsetFactor: { value: 0, min: 0, max: 1 },
+
+      bla: false
+    }
+  );
+
+  useEffect( () => { globalState.itemsCount = 1 }, [] );
+
+  // const [itemsCount, setItemsCount] = useState(state.itemsCount);
+  // useEffect( () => onItemsCountReady(itemsCount), [ itemsCount ] );
+
+  
+
+  // TODO: scroll condition to avoid extra calculation?
+  useFrame(
+    (state, delta) => {
+      const scrollRange = scroll.range(reservedRange, 1 - reservedRange);
+      const radiusOffset = cameraRadiusOffset * cameraRadiusOffsetFactor * scrollRange;
+      const radius = cameraRadius + radiusOffset;
+
+      // camera x
+      const angle = 2 * Math.PI * scrollRange;
+      const x = radius * Math.sin(angle);
+
+      // camera y
+      const totalHeight = state.viewport.height * globalState.itemsCount;
+      const y = - scrollRange * totalHeight + state.camera.position.y;
+
+      // camera z
+      const z = radius * Math.cos(angle);
+
+      const newCameraPosition = new THREE.Vector3(x, y, z);
+      
+      state.camera.position.set( ...newCameraPosition );
+      state.camera.lookAt( new THREE.Vector3(0, state.camera.position.y, 0) );
+    }
+  );
+
+  const calculateItemPosition = useCallback(
+    (index, distanceToOY) => {
+      const rangeValue = calculateItemLocalRange(index);
+
+      // x
+      const angle = 2 * Math.PI * rangeValue + Math.PI / globalState.itemsCount;
+      const x = distanceToOY * Math.sin(angle);
+
+      // y
+      const totalHeight = getThree().viewport.height * globalState.itemsCount;
+      const y = - rangeValue * totalHeight;
+
+      // z
+      const z = distanceToOY * Math.cos(angle);
+
+      return new THREE.Vector3(x, y, z);
+    },
+    [ globalState.itemsCount ]
+  );
+
+  const onItemVisibilityChangedHandler = useCallback(
+    (index, value) => {
+      // console.log(calculateItemGlobalRange(index));
+    },
+    []
+  );
+
+  const calculateItemRangeDelta = useCallback(
+    rangeDelta => rangeDelta * 0.5 * (1 - reservedRange) / globalState.itemsCount,
+    [reservedRange, globalState.itemsCount]
+  );
+
+  const calculateItemGlobalRange = useCallback(
+    (index) => transformItemRangeFromLocalToGlobal( calculateItemLocalRange(index) ),
+    [reservedRange, globalState.itemsCount, scroll.pages]
+  );
+
+  return (
+    <group position-y={ - getThree().viewport.height * 1.5 }>
+      {
+        [ ...Array(snap.itemsCount) ].map(
+          (v, i, a) => {
+            return (
+              <Item key={ Symbol( i ).toString() }
+                index={ i }
+                calculateItemPosition={ calculateItemPosition }
+                calculateItemGlobalRange={ calculateItemGlobalRange }
+                calculateItemRangeDelta={ calculateItemRangeDelta }
+                onItemVisibilityChanged={ onItemVisibilityChangedHandler }
+              /> 
+            )
+          }
+        )
+      }
+    </group>
+  );
+
+  function transformItemRangeFromLocalToGlobal(range) {
+    return reservedRange + range  * (1 - reservedRange) + 0.5 * (1 - reservedRange) / globalState.itemsCount;
+  }
+
+  function calculateItemLocalRange(order) {
+    return order / globalState.itemsCount;
+  }
+}
---
+++ src/Expirience/Models/Item.jsx
@@ -0,0 +1,292 @@
+import { useThree, useFrame } from '@react-three/fiber';
+import { useScroll, useIntersect, Center } from '@react-three/drei';
+import { useState, useEffect, useRef, useMemo, memo, useLayoutEffect } from 'react';
+import { useControls } from 'leva';
+import { useSpring, animated, easings, config } from '@react-spring/three';
+import * as THREE from 'three';
+
+export default memo(function Item({ 
+  index, 
+  calculateItemPosition,
+  calculateItemGlobalRange,
+  calculateItemRangeDelta,
+  onItemVisibilityChanged, 
+  ...props 
+}) {
+  console.log('item');
+
+  const scroll = useScroll();
+
+  const outerRef = useRef();
+  const innerRef = useRef();
+
+  const { 
+    distanceToOY,
+    itemRangeDelta,
+    itemRotationSpeed,
+    distanceModelModel,
+    distanceToMove
+  } = useControls(
+    'Item',
+    {
+      /* 
+        TODO: changing of this field works strangly,
+        and requieres one more re-render to be applied correctly
+      */
+      distanceToOY: { value: 5, min: 0, max: 25 },
+
+      itemRangeDelta: { value: 0.5, min: 0.1, max: 1 },
+      itemRotationSpeed: { value: 0.05, min: 0, max: 1 },
+      distanceModelModel: { value: 2, min: 1, max: 5 },
+      distanceToMove: { value: 3, min: 1, max: 5 }
+    }
+  );
+
+  const range = useMemo(
+    () => calculateItemGlobalRange(index), 
+    [index, calculateItemGlobalRange] 
+  );
+
+  const rangeDelta = useMemo(
+    () => calculateItemRangeDelta(itemRangeDelta), 
+    [itemRangeDelta, calculateItemRangeDelta] 
+  );
+
+  const defaultValues = useMemo(
+    () => ({ 
+      scale: 0.1, 
+      rotation: Array(3).fill( 2 * Math.PI * Math.random() ),
+      opacity: 0.2, 
+      position: [ ...calculateItemPosition(index, distanceToOY) ] 
+    }),
+    [calculateItemPosition, index, distanceToOY]
+  );
+  
+  const [itemSprings, itemApi] = useSpring(
+    () => ({
+      scale: defaultValues.scale,
+      rotation: defaultValues.rotation,
+      position: defaultValues.position,
+      opacity: defaultValues.opacity,
+
+      config: key => {
+        switch (key) {
+          case 'scale':
+            return { mass: 2, ...config.wobbly }
+
+          case 'rotation':
+            return { duration: 1000, easing: easings.easeOutCubic }
+
+          default: 
+            return {}
+        }
+      }
+    }),
+    [ defaultValues ]
+  );
+
+  const newPosition = useMemo(
+    () => {
+      const item = outerRef.current;
+      if (item === undefined)
+        return undefined;
+
+      const itemPostionProjectedOnOY = new THREE.Vector3(0, item.position.y, 0);
+      const directionFromItemPosition = item.position
+        .clone()
+        .sub(itemPostionProjectedOnOY);
+      const moveDirection = directionFromItemPosition.clone();
+      moveDirection.normalize();
+      moveDirection.applyEuler(
+        new THREE.Euler(
+          0,
+          Math.PI * 0.5,
+          0
+        )
+      );
+      moveDirection.multiplyScalar(distanceToMove);
+      const newPosition = item.position
+        .clone()
+        .add(moveDirection);
+
+      return newPosition;
+    },
+    [distanceToMove, outerRef.current]
+  );
+
+  const [isItemClicked, setItemClicked] = useState(false);
+
+  useEffect( 
+    () => {
+      itemApi.start(
+        isItemClicked
+        ?
+          { 
+            rotation: innerRef.current.rotation
+              .toArray()
+              .slice(0, -1)
+              .map(value => value + Math.PI), 
+              
+            position: [ ...newPosition ] 
+          }
+        :
+          { 
+            rotation: innerRef.current.rotation
+              .toArray()
+              .slice(0, -1)
+              .map(value => value - Math.PI), 
+
+            position: defaultValues.position 
+          }
+        );
+    }, 
+    [ isItemClicked ] 
+  ); 
+
+  const [isItemVisible, setIsItemVisible] = useState(false);
+
+  useEffect( 
+    () => {
+      if (isItemVisible) {
+        itemApi.start({ 
+          rotation: defaultValues.rotation.map(value => value + Math.PI / 1.5), 
+          scale: 1 
+        });
+      } else {
+        itemApi.set({ 
+          rotation: defaultValues.rotation, 
+          scale: defaultValues.scale 
+        });
+      }
+
+      onItemVisibilityChanged(index, isItemVisible);
+    }, 
+    [index, isItemVisible] 
+  );
+
+  const isModel_1_visible = useRef(false);
+
+  const model_1_ref = useIntersect( 
+    visible => {
+      isModel_1_visible.current = visible;
+
+      setItemVisibility();
+    } 
+  );
+
+  const isModel_2_visible = useRef(false);
+
+  const model_2_ref = useIntersect( 
+    visible => {
+      isModel_2_visible.current = visible;
+
+      setItemVisibility();
+    } 
+  ); 
+
+  const modelRefs = [model_1_ref, model_2_ref];
+
+  useLayoutEffect( 
+    () => {
+      modelRefs.forEach(ref => {
+        ref.current.rotation.set( ...Array(3).fill(Math.random() * 2 * Math.PI) );
+      });
+    }, 
+    [] 
+  );
+
+  const isItemHightlighted = useRef(false);
+
+  useFrame(
+    (state, delta) => {
+      const item = outerRef.current;
+
+      item.rotation.x += itemRotationSpeed * delta;
+      item.rotation.y += itemRotationSpeed * delta;
+      item.rotation.z += itemRotationSpeed * delta;
+
+      modelRefs.forEach(ref => {
+        const model = ref.current;
+
+        model.rotation.x += itemRotationSpeed * delta;
+        model.rotation.y += itemRotationSpeed * delta;
+        model.rotation.z += itemRotationSpeed * delta;
+      });
+
+      const isScrollRangeVisible = scroll.visible(range - rangeDelta, 2 * rangeDelta);
+      if (isScrollRangeVisible) {
+        
+        if (isItemHightlighted.current == false)
+          onItemHightlighted(true);
+
+
+      } else {
+
+        if (isItemHightlighted.current == true)
+          onItemHightlighted(false);
+
+      }
+    }
+  );
+
+  return (
+    <animated.group 
+      ref={ outerRef }
+      position={ itemSprings.position }
+    >
+      <animated.group
+        ref={ innerRef } 
+        rotation={ itemSprings.rotation }
+        scale={ itemSprings.scale }
+      >
+        <Center>
+          <animated.mesh 
+            position={ [0, 0, 0] }
+            ref={ model_1_ref }
+            onClick={ onModelClickHandler }
+            { ...props }
+          >
+            <animated.meshNormalMaterial
+              opacity={ itemSprings.opacity }
+              transparent
+            />
+            <boxGeometry />
+          </animated.mesh>
+
+          <animated.mesh 
+            position={ [0, distanceModelModel, 0] }
+            ref={ model_2_ref }
+            onClick={ onModelClickHandler }
+            { ...props }
+          >
+            <animated.meshNormalMaterial
+              opacity={ itemSprings.opacity }
+              transparent
+            />
+            <boxGeometry />
+          </animated.mesh>
+        </Center>
+      </animated.group> 
+    </animated.group> 
+  );
+
+  function onModelClickHandler(e) {    
+    setItemClicked(currentValue => !currentValue);
+  }
+
+  function onItemHightlighted(value) {
+    isItemHightlighted.current = value;
+
+    itemApi.start(
+      isItemHightlighted.current
+      ?
+        { opacity: 1 }
+      :
+        { opacity: defaultValues.opacity }
+    );
+  }
+
+  function setItemVisibility() {
+    setIsItemVisible(isModel_1_visible.current || isModel_2_visible.current)
+  }
+})
